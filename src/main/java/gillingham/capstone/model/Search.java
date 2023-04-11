a7acb2abd984e2b385690cca33d8af9890f5ea72
KennyG <kenneth.gillingham07@gmail.com>
2023-04-11 14:52:23 -0700

    added the search class to be used in the search bars on the main screen. will need to edit once i add the rest of the code for the project

--- src/main/java/gillingham/capstone/model/Search.java
+++ src/main/java/gillingham/capstone/model/Search.java
@@ -10,22 +10,22 @@ public class Search {
 
     public static ObservableList<Appointments> allAppointments = FXCollections.checkedObservableList();
 
-    public static ObservableList<Customer> lookUpCustomer_Name(String partialName){
+    public static ObservableList<Customer> lookUpCustomer_Name(String partialCustomerName){
         ObservableList<Customer> customerName = FXCollections.observableArrayList();
         ObservableList<Customer> allCustomers = Customer.getAllCustomers();
         for (Customer customer : allCustomers){
-            if (customer.getName().contains(partialName)){
+            if (customer.getName().contains(partialCustomerName)){
                 customerName.add(customer);
             }
         }
         return customerName;
     }
 
-    public static ObservableList<Appointments> lookUpAppointment_Name(String partialName){
+    public static ObservableList<Appointments> lookUpAppointment_Name(String partialAppointmentName){
         ObservableList<Appointments> appointmentName = FXCollections.observableArrayList();
         ObservableList<Appointments> allAppointments = Appointments.getAllAppointments();
         for (Appointments appointments : allAppointments){
-            if (appointments.getName().contains(partialName)){
+            if (appointments.getName().contains(partialAppointmentName)){
                 appointmentName.add(appointments);
             }
         }
